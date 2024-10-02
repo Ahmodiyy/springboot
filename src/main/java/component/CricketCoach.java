d7748b8ec01fc73ddfaf8e1e316d5113589c2bc0
Ahmodiyy <ahmodolaitan03@gmail.com>
2024-10-02 01:20:50 -1100

    lifecycle

--- src/main/java/component/CricketCoach.java
+++ src/main/java/component/CricketCoach.java
@@ -1,5 +1,7 @@
 package component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -7,8 +9,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Primary
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
+
+    @PostConstruct
+    public void afterInit(){
+        System.out.println("after construct...............");
+    }
+
+    @PreDestroy
+    public void beforeDestroy(){
+        System.out.println("before destroy.................");
+    }
     public CricketCoach() {
         System.out.println("cricket coach constructor...");
     }
