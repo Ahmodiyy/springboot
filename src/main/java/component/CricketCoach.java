be2beb240eaa12e28a347d60db0c8cebc10faa70
Ahmodiyy <ahmodolaitan03@gmail.com>
2024-10-02 00:12:27 -1100

    prototype scope

--- src/main/java/component/CricketCoach.java
+++ src/main/java/component/CricketCoach.java
@@ -1,10 +1,13 @@
 package component;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("cricket coach constructor...");
